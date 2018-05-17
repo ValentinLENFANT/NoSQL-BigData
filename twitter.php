38ffd81320d22f75c06947a96568cf7084681258
Valentin LENFANT <valentin.lenfant@hotmail.com>
2018-05-17 11:56:17 +0200

    Ajout form recherche et stat

--- twitter.php
+++ twitter.php
@@ -1,11 +1,14 @@
+<h1><a href="form.html">Chercher un nouveau mot clé</a></h1>
+
 <?php
 
 require_once('TwitterAPIExchange.php');
 require_once('token_api.php');
 require 'vendor/autoload.php';
 
+(string)$collectionToUse = $_POST['motcle'];
 $connection = new MongoDB\Client("mongodb://localhost:27017");
-$collection = $connection->projetBigData->twitter;
+$collection = $connection->projetBigData->$collectionToUse;
 
 $settings = ['oauth_access_token' => $oauth_access_token,
     'oauth_access_token_secret' => $oauth_access_token_secret,
@@ -13,7 +16,7 @@ $settings = ['oauth_access_token' => $oauth_access_token,
     'consumer_secret' => $consumer_secret];
 
 $url = "https://api.twitter.com/1.1/search/tweets.json";
-$getField = "?q=terrorisme&count=50&lang=fr&result_type=popular";
+$getField = "?q=".$collectionToUse."&count=50&lang=fr&result_type=popular";
 $requestMethod = "GET";
 
 $twitter = new TwitterAPIExchange($settings);
@@ -47,22 +50,85 @@ echo '<thead>
             </tr>
       </thead>';
 echo '<tbody>';
+
+$twitterWebClient = 0;
+$twitterForIphone = 0;
+$twitterMediaStudio = 0;
+
 foreach ($tweetsFromDatabase as $tweetFromDatabase) {
     $tweetFromDatabase = json_decode(json_encode($tweetFromDatabase), true);
     $tweet = explode('\'', $tweetFromDatabase[0]);
 
     echo '<tr>';
     // Date de rédaction du tweet
-    echo '<td style="border: 1px solid black;">' .$tweet[1]. '</td>';
+    echo '<td style="border: 1px solid black;">' . $tweet[1] . '</td>';
     // Auteur du tweet
-    echo '<td style="border: 1px solid black;">' .$tweet[9]. '</td>';
+    echo '<td style="border: 1px solid black;">' . $tweet[9] . '</td>';
+
+    if (substr($tweet[7], 0) == "Twitter Web Client") {
+        $twitterWebClient++;
+    } else if (substr($tweet[7], 0) == "Twitter for iPhone") {
+        $twitterForIphone++;
+    } else if (substr($tweet[7], 0) == "Media Studio") {
+        $twitterMediaStudio++;
+    }
     // Device utilisé pour rédiger le tweet
-    echo '<td style="border: 1px solid black;">' .$tweet[7]. '</td>';
+    echo '<td style="border: 1px solid black;">' . $tweet[7] . '</td>';
     // Contenu du tweet
-    echo '<td style="border: 1px solid black;">' .$tweet[3]. '</td>';
+    echo '<td style="border: 1px solid black;">' . $tweet[3] . '</td>';
     // URL du tweet
-    echo '<td style="border: 1px solid black;">' .$tweet[5]. '</td>';
+    echo '<td style="border: 1px solid black;">' . $tweet[5] . '</td>';
     echo '</tr>';
 }
+
+$eachSource = [$twitterWebClient, $twitterForIphone, $twitterMediaStudio];
+
 echo '</tbody>';
 echo '</table>';
+
+?>
+
+<script src="amcharts.js" type="text/javascript"></script>
+<script src="pie.js" type="text/javascript"></script>
+
+<script>
+    var chart;
+    var legend;
+
+    var chartData = [
+        {
+            "devices": "Web Client",
+            "value": "<?php echo $eachSource[0];?>"
+        },
+        {
+            "devices": "Iphone",
+            "value": "<?php echo $eachSource[1];?>"
+        },
+        {
+            "devices": "Media Studio",
+            "value": "<?php echo $eachSource[2];?>"
+        }
+    ];
+
+    AmCharts.ready(function () {
+        // PIE CHART
+        chart = new AmCharts.AmPieChart();
+        chart.dataProvider = chartData;
+        chart.titleField = "devices";
+        chart.valueField = "value";
+        chart.outlineColor = "#FFFFFF";
+        chart.outlineAlpha = 0.8;
+        chart.outlineThickness = 2;
+        chart.balloonText = "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>";
+        // this makes the chart 3D
+        chart.depth3D = 15;
+        chart.angle = 30;
+
+        // WRITE
+        chart.write("chartdiv");
+    });
+</script>
+
+<body>
+<div id="chartdiv" style="width: 100%; height: 400px;"></div>
+</body>
